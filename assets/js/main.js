27abb95724ffb106e6a9fe4ace91daac6b446bdf
Viraj Kadam <hivirajkadam@gmail.com>
2021-04-09 02:28:22 +0530

    'Viraj'

---
+++ assets/js/main.js
@@ -0,0 +1,130 @@
+!(function($) {
+	"use strict";
+
+	// Preloader
+	$(window).on('load', function() {
+		if ($('#preloader').length) {
+			$('#preloader').delay(50).fadeOut('slow', function() {
+				$(this).remove();
+			});
+		}
+	});
+
+	// Hero typed
+	if ($('.typed').length) {
+		var typed_strings = $(".typed").data('typed-items');
+		typed_strings = typed_strings.split(',')
+		new Typed('.typed', {
+			strings: typed_strings,
+			loop: true,
+			typeSpeed: 120,
+			backSpeed: 80,
+			backDelay: 2000
+		});
+	}
+
+	// Smooth scroll for the navigation menu and links with .scrollto classes
+	$(document).on('click', '.nav-menu a, .scrollto', function(e) {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			if (target.length) {
+				e.preventDefault();
+
+				var scrollto = target.offset().top;
+
+				$('html, body').animate({
+					scrollTop: scrollto
+				}, 1500, 'easeInOutExpo');
+
+				if ($(this).parents('.nav-menu, .mobile-nav').length) {
+					$('.nav-menu .active, .mobile-nav .active').removeClass('active');
+					$(this).closest('li').addClass('active');
+				}
+
+				if ($('body').hasClass('mobile-nav-active')) {
+					$('body').removeClass('mobile-nav-active');
+					$('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+				}
+				return false;
+			}
+		}
+	});
+
+	// Activate smooth scroll on page load with hash links in the url
+	$(document).ready(function() {
+		if (window.location.hash) {
+			var initial_nav = window.location.hash;
+			if ($(initial_nav).length) {
+				var scrollto = $(initial_nav).offset().top;
+				$('html, body').animate({
+					scrollTop: scrollto
+				}, 1500, 'easeInOutExpo');
+			}
+		}
+	});
+
+	$(document).on('click', '.mobile-nav-toggle', function(e) {
+		$('body').toggleClass('mobile-nav-active');
+		$('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+	});
+
+	$(document).click(function(e) {
+		var container = $(".mobile-nav-toggle");
+		if (!container.is(e.target) && container.has(e.target).length === 0) {
+			if ($('body').hasClass('mobile-nav-active')) {
+				$('body').removeClass('mobile-nav-active');
+				$('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+			}
+		}
+	});
+
+	// Navigation active state on scroll
+	var nav_sections = $('section');
+	var main_nav = $('.nav-menu, #mobile-nav');
+
+	$(window).on('scroll', function() {
+		var cur_pos = $(this).scrollTop() + 300;
+
+		nav_sections.each(function() {
+			var top = $(this).offset().top,
+				bottom = top + $(this).outerHeight();
+
+			if (cur_pos >= top && cur_pos <= bottom) {
+				if (cur_pos <= bottom) {
+					main_nav.find('li').removeClass('active');
+				}
+				main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+			}
+			if (cur_pos < 200) {
+				$(".nav-menu ul:first li:first").addClass('active');
+			}
+		});
+	});
+
+	// Back to top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
+
+	$('.back-to-top').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	});
+
+	// Skills section
+	$('.skills-content').waypoint(function() {
+		$('.progress .progress-bar').each(function() {
+			$(this).css("width", $(this).attr("aria-valuenow") + '%');
+		});
+	}, {
+		offset: '80%'
+	});
+
+
+})(jQuery);
---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+$base_url = $_SERVER['REQUEST_URI'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>Viraj Kadam | Who is Viraj Kadam | Contact Viraj Kadam</title>
+	
+	<meta name="keywords" content="Viraj Kadam, Contact Viraj Kadam, About Viraj Kadam, Viraj Kadam Sangli">
+	<meta name="description" content="Viraj Kadam is an indian software developer, nature explorer and dreamer. Viraj also likes try new food and read technology blogs. IOT and Programming being his favourite topics of reading viraj kadam likes to write articles sometimes.">
+
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="never">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="author" content="Viraj Kadam">
+	<meta name="revisit-after" content="7 days">
+	<meta name="distribution" content="web">
+	<meta name="rating" content="general">
+	<meta name="robots" content="index, follow">
+	<meta name="apple-mobile-web-app-capable" content="YES">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="identifier-url" content="URL/">
+	<meta name="distribution" content="global">
+	<meta name="googlebot" content="all">
+	<meta name="geo.placename" content="india">
+	<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+	<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+
+	<link rel="canonical" href="https://virajkadam.com/index.php">
+	<link rel="shortlink" href="https://virajkadam.com/index.php">
+
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Viraj Kadam | Who is Viraj Kadam | Contact Viraj Kadam">
+	<meta property="og:description" content="Viraj Kadam is an indian software developer, nature explorer and dreamer. Viraj also likes try new food and read technology blogs. IOT and Programming being his favourite topics of reading viraj kadam likes to write articles sometimes.">
+	<meta property="og:url" content="https://virajkadam.com/">
+	<meta property="og:site_name" content="Viraj Kadam">
+	<meta property="article:publisher" content="https://www.facebook.com/virajkadam1992/">
+	<meta property="article:updated_time" content="1589023717">
+	<meta property="og:image" itemprop="image" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta property="og:image:url" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta property="og:image:secure" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta property="og:image:secure_url" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta property="og:image:alt" content="Viraj Kadam Image">
+	<meta property="og:image:width" content="720">
+	<meta property="og:image:height" content="602">
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="Viraj Kadam | Who is Viraj Kadam | Contact Viraj Kadam">
+	<meta name="twitter:description" content="Viraj Kadam is an indian software developer, nature explorer and dreamer. Viraj also likes try new food and read technology blogs. IOT and Programming being his favourite topics of reading viraj kadam likes to write articles sometimes.">
+	<meta name="twitter:image" content="https://virajkadam.com/assets/img/profile-img.jpg">
+
+	<link href="assets/img/favicon.png" rel="icon">
+	<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300" rel="stylesheet">
+
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+	<header id="header" class="d-flex flex-column justify-content-center">
+		<nav class="nav-menu">
+			<ul>
+				<li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
+				<li><a href="#about"><i class="bx bx-user"></i> <span>About</span></a></li>
+				<li><a href="#skills"><i class="bx bx-server"></i> <span>Skills</span></a></li>
+				<li><a href="#contact"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section id="hero" class="d-flex flex-column justify-content-center">
+		<div class="container" data-aos="zoom-in" data-aos-delay="100">
+			<h1>Viraj Kadam</h1>
+			<p>I'm <span class="typed" data-typed-items="Software Developer, Nature Explorer, Dreamer"></span></p>
+			<div class="social-links">
+				<a href="https://www.instagram.com/virajkadam92" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+				<a href="https://www.quora.com/profile/Viraj-Kadam-5" target="_blank" class="google-plus"><i class="bx bxl-quora"></i></a>
+				<a href="https://www.linkedin.com/in/virajkadam92" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+			</div>
+		</div>
+	</section>
+
+
+	<main id="main">
+		<section id="about" class="about">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>About Viraj Kadam</h2>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+					</div>
+					<div class="col-lg-8 pt-4 pt-lg-0 content">
+						<h3>Web Development to Wild Explorer</h3>
+						<p class="font-italic"><br>
+							Viraj Kadam is an indian software developer, nature explorer and dreamer. Viraj also likes try new food and read technology blogs. IOT and Programming being his favourite topics of reading viraj kadam likes to write articles sometimes.
+						</p>
+						<p class="font-italic"><br>
+							Viraj Kadam loves teaching and programming by heart and by prooffesion too. Also being a nature lover Viraj Kadam loves to wander and trek in natures womb.
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+
+		<section id="skills" class="skills section-bg">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Skills</h2>
+					<p>Viraj Kadam likes imporving his skills and appriciate others skills as skills are representations of individuals obsession and hardwork. Skills are ment to progrress an individual.</p>
+				</div>
+
+				<div class="row skills-content">
+
+					<div class="col-lg-6">
+
+						<div class="progress">
+							<span class="skill">Web Development <i class="val">90%</i></span>
+							<div class="progress-bar-wrap">
+								<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+
+						<div class="progress">
+							<span class="skill">Good Communication <i class="val">75%</i></span>
+							<div class="progress-bar-wrap">
+								<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-lg-6">
+
+						<div class="progress">
+							<span class="skill">Product Manegement <i class="val">80%</i></span>
+							<div class="progress-bar-wrap">
+								<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+
+						<div class="progress">
+							<span class="skill">Watching Movies  <i class="val">95%</i></span>
+							<div class="progress-bar-wrap">
+								<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-lg-12">
+						<div class="progress">
+							<span class="skill">Sarcasm <i class="val">100%</i></span>
+							<div class="progress-bar-wrap">
+								<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+
+
+		<section id="contact" class="contact">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Contact</h2>
+				</div>
+
+				<div class="row mt-1">
+
+					<div class="col-lg-4">
+						<div class="info">
+							<div class="address">
+								<i class="icofont-google-map"></i>
+								<h4>Location:</h4>
+								<p>Sangli, Pune</p>
+							</div>
+
+							<div class="email">
+								<i class="icofont-envelope"></i>
+								<h4>Email:</h4>
+								<p>HiVirajKadam@gmail.com</p>
+							</div>
+
+							<div class="phone">
+								<i class="icofont-whatsapp"></i>
+								<h4>Whatsapp:</h4>
+								<p>+91 8806431723</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+
+	<footer id="footer">
+		<div class="container">
+			<h3>Viraj Kadam</h3>
+			<p>Sofwtare Developer | Nature Explorer | Dreamer</p>
+			<div class="social-links">
+				<a href="https://www.instagram.com/virajkadam92" target="_blank" class="instagram">
+					<i class="bx bxl-instagram"></i>
+				</a>
+				<a href="https://www.quora.com/profile/Viraj-Kadam-5" target="_blank" class="google-plus">
+					<i class="bx bxl-quora"></i>
+				</a>
+				<a href="https://www.linkedin.com/in/virajkadam92" target="_blank" class="linkedin">
+					<i class="bx bxl-linkedin"></i>
+				</a>
+			</div>
+			<div class="credits"> Designed by <a href="<?=$base_url?>#about">Viraj Kadam</a> himself </div>
+		</div>
+	</footer>
+
+	<a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+	<div id="preloader"></div>
+
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+	<script src="assets/vendor/php-email-form/validate.js"></script>
+	<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+	<script src="assets/vendor/typed.js/typed.min.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
