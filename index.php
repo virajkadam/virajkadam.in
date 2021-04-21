c57faccde040dab23a293de905fa6e26f8ac3d09
Viraj Kadam <hivirajkadam@gmail.com>
2021-04-22 02:58:01 +0530

    'Viraj'

--- index.php
+++ index.php
@@ -46,10 +46,10 @@ $base_url = $_SERVER['REQUEST_URI'];
 	<meta property="og:site_name" content="Viraj Kadam">
 	<meta property="article:publisher" content="https://www.facebook.com/virajkadam1992/">
 	<meta property="article:updated_time" content="1589023717">
-	<meta property="og:image" itemprop="image" content="https://virajkadam.com/assets/img/profile-img.jpg">
-	<meta property="og:image:url" content="https://virajkadam.com/assets/img/profile-img.jpg">
-	<meta property="og:image:secure" content="https://virajkadam.com/assets/img/profile-img.jpg">
-	<meta property="og:image:secure_url" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta property="og:image" itemprop="image" content="https://virajkadam.com/assets/img/viraj-kadam.jpg">
+	<meta property="og:image:url" content="https://virajkadam.com/assets/img/viraj-kadam.jpg">
+	<meta property="og:image:secure" content="https://virajkadam.com/assets/img/viraj-kadam.jpg">
+	<meta property="og:image:secure_url" content="https://virajkadam.com/assets/img/viraj-kadam.jpg">
 	<meta property="og:image:alt" content="Viraj Kadam Image">
 	<meta property="og:image:width" content="720">
 	<meta property="og:image:height" content="602">
@@ -57,30 +57,28 @@ $base_url = $_SERVER['REQUEST_URI'];
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:title" content="Viraj Kadam | Who is Viraj Kadam | Contact Viraj Kadam">
 	<meta name="twitter:description" content="Viraj Kadam is an indian software developer, nature explorer and dreamer. Viraj also likes try new food and read technology blogs. IOT and Programming being his favourite topics of reading viraj kadam likes to write articles sometimes.">
-	<meta name="twitter:image" content="https://virajkadam.com/assets/img/profile-img.jpg">
+	<meta name="twitter:image" content="https://virajkadam.com/assets/img/viraj-kadam.jpg">
 
-	<link href="assets/img/favicon.png" rel="icon">
-	<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="<?=$base_url?>/assets/img/favicon.png" rel="icon">
+	<link href="<?=$base_url?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300" rel="stylesheet">
-
-	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-	<link href="assets/css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="<?=$base_url?>/assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
 
-	<button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+	<button type="button" class="mobile-nav-toggle d-xl-none"><i class="fa fa-bars"></i></button>
 
 	<header id="header" class="d-flex flex-column justify-content-center">
 		<nav class="nav-menu">
 			<ul>
-				<li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
-				<li><a href="#about"><i class="bx bx-user"></i> <span>About</span></a></li>
-				<li><a href="#skills"><i class="bx bx-server"></i> <span>Skills</span></a></li>
-				<li><a href="#contact"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+				<li class="active"><a href="#hero"><i class="fa fa-home"></i> <span>Home</span></a></li>
+				<li><a href="#about"><i class="fa fa-user"></i> <span>About</span></a></li>
+				<li><a href="#skills"><i class="fa fa-lightbulb-o"></i> <span>Skills</span></a></li>
+				<li><a href="#contact"><i class="fa fa-comments"></i> <span>Contact</span></a></li>
 			</ul>
 		</nav>
 	</header>
@@ -90,9 +88,9 @@ $base_url = $_SERVER['REQUEST_URI'];
 			<h1>Viraj Kadam</h1>
 			<p>I'm <span class="typed" data-typed-items="Software Developer, Nature Explorer, Dreamer"></span></p>
 			<div class="social-links">
-				<a href="https://www.instagram.com/virajkadam92" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
-				<a href="https://www.quora.com/profile/Viraj-Kadam-5" target="_blank" class="google-plus"><i class="bx bxl-quora"></i></a>
-				<a href="https://www.linkedin.com/in/virajkadam92" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+				<a href="https://www.instagram.com/virajkadam92" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.quora.com/profile/Viraj-Kadam-5" target="_blank" class="google-plus"><i class="fa fa-quora"></i></a>
+				<a href="https://www.linkedin.com/in/virajkadam92" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a>
 			</div>
 		</div>
 	</section>
@@ -108,7 +106,7 @@ $base_url = $_SERVER['REQUEST_URI'];
 
 				<div class="row">
 					<div class="col-lg-4">
-						<img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+						<img src="assets/img/viraj-kadam.jpg" class="img-fluid" alt="">
 					</div>
 					<div class="col-lg-8 pt-4 pt-lg-0 content">
 						<h3>Web Development to Wild Explorer</h3>
@@ -198,21 +196,15 @@ $base_url = $_SERVER['REQUEST_URI'];
 					<div class="col-lg-4">
 						<div class="info">
 							<div class="address">
-								<i class="icofont-google-map"></i>
+								<i class="fa fa-street-view"></i>
 								<h4>Location:</h4>
 								<p>Sangli, Pune</p>
 							</div>
 
 							<div class="email">
-								<i class="icofont-envelope"></i>
+								<i class="fa fa-envelope-open-o"></i>
 								<h4>Email:</h4>
 								<p>HiVirajKadam@gmail.com</p>
-							</div>
-
-							<div class="phone">
-								<i class="icofont-whatsapp"></i>
-								<h4>Whatsapp:</h4>
-								<p>+91 8806431723</p>
 							</div>
 						</div>
 					</div>
@@ -228,28 +220,27 @@ $base_url = $_SERVER['REQUEST_URI'];
 			<p>Sofwtare Developer | Nature Explorer | Dreamer</p>
 			<div class="social-links">
 				<a href="https://www.instagram.com/virajkadam92" target="_blank" class="instagram">
-					<i class="bx bxl-instagram"></i>
+					<i class="fa fa-instagram"></i>
 				</a>
 				<a href="https://www.quora.com/profile/Viraj-Kadam-5" target="_blank" class="google-plus">
-					<i class="bx bxl-quora"></i>
+					<i class="fa fa-quora"></i>
 				</a>
 				<a href="https://www.linkedin.com/in/virajkadam92" target="_blank" class="linkedin">
-					<i class="bx bxl-linkedin"></i>
+					<i class="fa fa-linkedin"></i>
 				</a>
 			</div>
 			<div class="credits"> Designed by <a href="<?=$base_url?>#about">Viraj Kadam</a> himself </div>
 		</div>
 	</footer>
 
-	<a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+	<a href="#" class="back-to-top"><i class="fa fa-chevron-circle-up"></i></a>
 	<div id="preloader"></div>
 
-	<script src="assets/vendor/jquery/jquery.min.js"></script>
-	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-	<script src="assets/vendor/php-email-form/validate.js"></script>
-	<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-	<script src="assets/vendor/typed.js/typed.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
 	<script src="assets/js/main.js"></script>
 
 </body>
