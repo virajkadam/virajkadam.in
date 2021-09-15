17800f2cfb8d1d16c31a4f8112e3dc7a00a47e7b
Viraj Kadam <HiVirajKadam@gmail.com>
2021-09-16 02:15:50 +0530

    'Viraj Local'

--- project/pinomic-calculator.php
+++ project/pinomic-calculator.php
@@ -9,7 +9,7 @@ require_once('../config.php');
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Stock Dividend Calculator</title>
+		<title>Pinomic Calculator</title>
 
 		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="https://getbootstrap.com/docs/4.0/examples/floating-labels/floating-labels.css" rel="stylesheet">
