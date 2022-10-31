a8cbdd334d1cf5bded7da25b96e6dfe305ca4d2c
Viraj Kadam <hivirajkadam@gmail.com>
2022-11-01 01:38:49 +0530

    'Viraj: First Commit'

---
+++ command.sh
@@ -0,0 +1,18 @@
+git init
+
+git config core.fileMode false
+git config core.autocrlf false
+
+git add .
+git commit -m "'$1'"
+
+git rm -r --cached .
+git add .
+git commit -m "'$1'"
+
+git pull origin master
+
+chmod -R 777 ../virajkadam.com/
+chown -R www-data:www-data ../virajkadam.com/
+
+git push
